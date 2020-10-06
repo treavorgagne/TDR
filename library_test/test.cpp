9796ce07519b4548152315e5b93b3212a3f871eb
James Lacroix <lacroix.james@icloud.com>
2020-10-05 18:02:49 -0700

    Created test file to verify SFML install

---
+++ library_test/test.cpp
@@ -0,0 +1,14 @@
+
+#include <iostream>
+
+#include <SFML/Network.hpp>
+
+int main(){
+
+	std::cout << "Hello world!\n";
+
+
+	sf::TcpListener listener;
+
+	std::cout << "If you see this message, SFML network library was succesfully linked!\n";
+}
